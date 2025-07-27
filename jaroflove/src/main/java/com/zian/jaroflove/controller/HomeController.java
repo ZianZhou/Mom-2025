1f2c2a3cf9667d8a99f2dc38ac31bfac6ff784b8
Zian Zhou <zianzhou@zians-mbp.home>
2025-07-27 11:55:11 -0700

    Allow add

--- jaroflove/src/main/java/com/zian/jaroflove/controller/HomeController.java
+++ jaroflove/src/main/java/com/zian/jaroflove/controller/HomeController.java
@@ -1,13 +1,34 @@
 package com.zian.jaroflove.controller;
 
+import com.zian.jaroflove.model.Message;
+import com.zian.jaroflove.service.MessageService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HomeController {
 
+    private final MessageService service;
+
+    public HomeController(MessageService service) {
+        this.service = service;
+    }
+
     @GetMapping("/")
     public String home() {
-        return "index"; // returns templates/index.html
+        return "index"; // show random message
+    }
+
+    @GetMapping("/add")
+    public String showForm(Model model) {
+        model.addAttribute("message", new Message());
+        return "add"; // renders add.html
+    }
+
+    @PostMapping("/add")
+    public String submitForm(@ModelAttribute Message message) {
+        service.addMessage(message.getText());
+        return "redirect:/"; // go back to home
     }
 }
